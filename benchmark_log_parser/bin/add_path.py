46ba5e4406d07041fbc2019d4558c0165cee985f
xiaoyekanren <876670773@qq.com>
2022-03-14 15:37:55 +0800

    add print_path to addpath.py, add step to checkout benchmark_log_parser

--- benchmark_log_parser/bin/add_path.py
+++ benchmark_log_parser/bin/add_path.py
@@ -2,4 +2,23 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+def add_python_path():
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+
+def print_python_path():
+    for i in sys.path:
+        print(i)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('you can add only one parameter')
+        exit()
+    elif sys.argv[1] == 'add_python_path':
+        add_python_path()
+    elif sys.argv[1] == 'print_python_path':
+        print_python_path()
+    else:
+        exit()
