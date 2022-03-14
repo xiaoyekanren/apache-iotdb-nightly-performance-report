d644dedd4bfe5d8608f194b807a9ac337f0fd40e
xiaoyekanren <876670773@qq.com>
2022-03-14 18:14:50 +0800

    add python break

--- benchmark_log_parser/bin/add_path.py
+++ benchmark_log_parser/bin/add_path.py
@@ -4,6 +4,9 @@ import sys
 
 
 def add_python_path():
+    print(os.path.dirname(__file__))
+    print(os.path.join(os.path.dirname(__file__), '../'))
+    print(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 
